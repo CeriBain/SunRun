cb10b9ae6a0dbfde5a0ed7a660eeb9595100a970
Ceri Bain <ceribain@Ceris-MacBook-Pro.local>
2026-05-07 12:40:49 +0100

    create project scaffold with react, vite, tailwind, leaflet

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Routes, Route } from 'react-router-dom'
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<div className="p-4 text-2xl font-bold">SunRun</div>} />
+    </Routes>
+  )
+}
+
+export default App
