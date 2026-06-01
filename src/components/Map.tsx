f8d8c9421e948a72b0d8f675082eef48123f4010
Ceri Bain <ceribain@Ceris-MBP.lan>
2026-06-01 16:36:23 +0100

    set state for Map component

---
+++ src/components/Map.tsx
@@ -0,0 +1,9 @@
+import { MapContainer, TileLayer } from 'react-leaflet'
+import { useLocation } from '../hooks/useLocation'
+
+export function Map() {
+  const { coords, loading, error } = useLocation()
+  if (loading) return <div className="p-4">Locating you, hang tight...</div>
+  if (error) return <div className="p-4 text-red-500">{error}"</div>
+  if (!coords) return null
+}
